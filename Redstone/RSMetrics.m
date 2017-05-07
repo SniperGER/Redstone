a7d9a34f0749ba7329223a74e89179497e032b05
SniperGER <janikschmidt@icloud.com>
2017-05-07 09:30:43 +0200

    Added tile sizes for 3.5"/4" and 4.7"

--- Redstone/RSMetrics.m
+++ Redstone/RSMetrics.m
@@ -2,7 +2,7 @@
 
 @implementation RSMetrics
 
-int columns = 3;
+int columns;
 
 + (void)initialize {
 	columns = 3;
@@ -21,6 +21,50 @@ int columns = 3;
 				return CGSizeMake(269, 269);
 			}
 			break;
+		case 375:
+			if (columns == 3) {
+				if (size == 1) {
+					return CGSizeMake(57, 57);
+				} else if (size == 2) {
+					return CGSizeMake(119, 119);
+				} else if (size == 3) {
+					return CGSizeMake(243, 119);
+				} else if (size == 4) {
+					return CGSizeMake(243, 243);
+				}
+			} else if (columns == 2) {
+				if (size == 1) {
+					return CGSizeMake(88, 88);
+				} else if (size == 2) {
+					return CGSizeMake(181, 181);
+				} else if (size == 3) {
+					return CGSizeMake(367, 181);
+				} else if (size == 4) {
+					return CGSizeMake(367, 367);
+				}
+			}
+		case 320:
+			if (columns == 3) {
+				if (size == 1) {
+					return CGSizeMake(47.83333, 47.83333);
+				} else if (size == 2) {
+					return CGSizeMake(100.66666, 100.66666);
+				} else if (size == 3) {
+					return CGSizeMake(206.33333, 100.66666);
+				} else if (size == 4) {
+					return CGSizeMake(206.33333, 206.33333);
+				}
+			} else if (columns == 2) {
+				if (size == 1) {
+					return CGSizeMake(74.25, 74.25);
+				} else if (size == 2) {
+					return CGSizeMake(153.5, 153.5);
+				} else if (size == 3) {
+					return CGSizeMake(312, 153.5);
+				} else if (size == 4) {
+					return CGSizeMake(312, 312);
+				}
+			}
 		default: break;
 	}
 	
