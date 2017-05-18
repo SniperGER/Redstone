b975dea51162fe77d59708f103ab7ad924170964
SniperGER <janikschmidt@icloud.com>
2017-05-19 00:59:38 +0200

    Added missing tile icon sizes

--- Redstone/Core/RSMetrics.m
+++ Redstone/Core/RSMetrics.m
@@ -75,11 +75,49 @@ int columns;
 	switch ((int)screenWidth) {
 		case 414:
 			if (size == 1) {
-				return CGSizeMake(31.75, 31.75);
+				return CGSizeMake(32, 32);
 			} else if (size == 2 || size == 3) {
 				return CGSizeMake(44, 44);
 			} else if (size == 4) {
-				return CGSizeMake(89.66666, 89.66666);
+				return CGSizeMake(90, 90);
+			}
+			break;
+		case 375:
+			if (columns == 3) {
+				if (size == 1) {
+					return CGSizeMake(28.5, 28.5);
+				} else if (size == 2 || size == 3) {
+					return CGSizeMake(40, 40);
+				} else if (size == 4) {
+					return CGSizeMake(81, 81);
+				}
+			} else if (columns == 2){
+				if (size == 1) {
+					return CGSizeMake(44, 44);
+				} else if (size == 2 || size == 3) {
+					return CGSizeMake(60, 60);
+				} else if (size == 4) {
+					return CGSizeMake(122, 122);
+				}
+			}
+			break;
+		case 320:
+			if (columns == 3) {
+				if (size == 1) {
+					return CGSizeMake(24, 24);
+				} else if (size == 2 || size == 3) {
+					return CGSizeMake(34, 34);
+				} else if (size == 4) {
+					return CGSizeMake(70, 70);
+				}
+			} else if (columns == 2){
+				if (size == 1) {
+					return CGSizeMake(38, 38);
+				} else if (size == 2 || size == 3) {
+					return CGSizeMake(50, 50);
+				} else if (size == 4) {
+					return CGSizeMake(104, 104);
+				}
 			}
 			break;
 		default: break;
